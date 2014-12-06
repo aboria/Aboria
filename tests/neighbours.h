5b7cbcac66fa3d2ed585280c6d74185d14cadc34
martinjrobins <martinjrobins@gmail.com>
2014-12-06 00:11:36 +0000

    added neighbours test

---
+++ tests/neighbours.h
@@ -0,0 +1,69 @@
+/*
+ * neighbours.h
+ *
+ * Copyright 2014 Martin Robinson
+ *
+ * This file is part of Aboria.
+ *
+ * Aboria is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Aboria is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Aboria.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  Created on: 27 Nov 2014
+ *      Author: robinsonm
+ */
+
+#ifndef NEIGHBOURS_H_
+#define NEIGHBOURS_H_
+
+#include <cxxtest/TestSuite.h>
+
+#define LOG_LEVEL 1
+#include "Aboria.h"
+
+using namespace Aboria;
+
+
+class NeighboursTest : public CxxTest::TestSuite {
+public:
+    void test_create_(void) {
+    	typedef Particles<std::tuple<double> > Test_type;
+    	Test_type test;
+    	Vect3d min(-1,-1,-1);
+    	Vect3d max(1,1,1);
+    	Vect3d periodic(true,true,true);
+    	double diameter = 0.1;
+    	test.init_neighbour_search(min,max,diameter,periodic);
+    	Test_type::value_type p;
+
+    	p.set_position(Vect3d(0,0,0));
+    	test.push_back(p);
+
+    	auto tpl = test.get_neighbours(Vect3d(diameter/2,diameter/2,0));
+    	TS_ASSERT_EQUALS(tpl.size(),1);
+
+    	tpl = test.get_neighbours(Vect3d(2*diameter,0,0));
+    	TS_ASSERT_EQUALS(tpl.size(),0);
+
+    	p.set_position(Vect3d(diameter/2,0,0));
+    	test.push_back(p);
+
+    	tpl = test.get_neighbours(Vect3d(1.1*diameter,0,0));
+    	TS_ASSERT_EQUALS(tpl.size(),1);
+    	const Test_type::value_type &pfound = std::get<0>(*tpl.begin());
+    	TS_ASSERT_EQUALS(pfound.get_id(),test[1].get_id());
+    }
+};
+
+
+
+#endif /* NEIGHBOURS_H_ */
