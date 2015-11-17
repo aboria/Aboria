643ff7da86ed37b2b20b2e356d52e22d591e05e2
martinjrobins <martinjrobins@gmail.com>
2015-11-17 13:25:10 +0000

    added util function to create particles with rejection sampling

--- src/Utils.h
+++ src/Utils.h
@@ -12,6 +12,35 @@
 
 namespace Aboria {
 
+template<typename PTYPE, typename GTYPE, typename RNDGTYPE>
+void create_n_particles_with_rejection_sampling(const unsigned int n, PTYPE &particles, const GTYPE &generator_fun, const Vect3d &low, const Vect3d &high, RNDGTYPE &generator) {
+
+    std::uniform_real_distribution<double> uni(0,1);
+    const Vect3d h = (high-low)/1000;
+    double max_generator_fun = 0;
+    assert(h[0]>0);
+    for (double x = low[0]+h[0]/2; x<high[0]; x += h[0]) {
+        double yinc = h[1];
+        if (yinc == 0) yinc = h[0];
+        for (double y = low[1]+h[1]/2; y<=high[1]; y += yinc) {
+            double zinc = h[2];
+            if (zinc == 0) zinc = h[0];
+            for (double z = low[2]+h[2]/2; z<=high[2]; z += zinc) {
+                if (generator_fun(Vect3d(x,y,z)) > max_generator_fun) max_generator_fun = generator_fun(Vect3d(x,y,z));
+            }
+        }
+    }
+    for (int i = 0; i < n; ++i) {
+        Vect3d r = Vect3d(uni(generator),uni(generator),uni(generator))*(high-low) + low;
+        while (uni(generator) > generator_fun(r)/max_generator_fun) {
+            r = Vect3d(uni(generator),uni(generator),uni(generator))*(high-low) + low;
+        }
+        particles.push_back(r);
+    }
+}
+
+
+
 template<typename T>
 ptr<std::vector<double> > radial_distribution_function(ptr<Particles<T> > particles,
 							const double min, const double max,
