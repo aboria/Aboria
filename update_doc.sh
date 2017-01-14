4b0c12748880ce150378f56071431d54d1422e5b
Martin Robinson <martinjrobins@gmail.com>
2017-01-14 07:41:41 +0000

    update doc

--- update_doc.sh
+++ update_doc.sh
@@ -2,13 +2,20 @@
 
 git fetch origin gh-pages
 git checkout gh-pages
-make generate_doc
-cp -R doc/html/* .
-git add *.html
-git add Aboria/*
-git add aboria/*
-git add Eigen/*
-git add index/*
-git commit -m "Updated documentation by TravisCI"
-git push
-git checkout master
+if [ $? -eq 0 ]; then
+    make generate_doc
+    cp -R doc/html/* .
+    git add *.html
+    git add standalone_HTML.manifest
+    git add Aboria/*
+    git add aboria/*
+    git add Eigen/*
+    git add index/*
+    git commit -m "Updated documentation by TravisCI"
+    git push
+    git checkout master
+    echo OK
+else
+    echo FAIL
+fi
+
