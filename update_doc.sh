e47588c4b1193db876009f3b2280fa2c3a0a33a3
Martin Robinson <martinjrobins@gmail.com>
2017-01-14 05:49:58 +0000

    see if we can use travis trusty

--- update_doc.sh
+++ update_doc.sh
@@ -1,8 +1,12 @@
 #!/usr/bash
 
 make generate_doc
-checkout gh-pages
+git checkout gh-pages
 cp -R doc/html/* .
-git add *
+git add *.html
+git add Aboria/*
+git add aboria/*
+git add Eigen/*
+git add index/*
 git commit -m "Updated documentation by TravisCI"
 git push
