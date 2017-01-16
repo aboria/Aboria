7a0b4f9fca5885d11de5d64b6cd74363b2a3033c
Martin Robinson <martinjrobins@gmail.com>
2017-01-16 12:38:19 +0000

    fix chunking and add right stuff

--- update_doc.sh
+++ update_doc.sh
@@ -6,11 +6,12 @@ git checkout gh-pages
 if [ $? -eq 0 ]; then
     cp -R doc/html-website/* .
     git add *.html
-    git add standalone_HTML.manifest
-    git add Aboria/*
-    git add aboria/*
-    git add Eigen/*
-    git add index/*
+    git add aboria/\*.html
+    git add Aboria/\*.html
+    git add css/\*.css
+    git add images/\*.png
+    git add images/\*.jpg
+    git add HTML.manifest
     git commit -m "Updated documentation by TravisCI"
     git push
     git checkout master
