dc2c8b158aa6b9b76bd6a07d365338690aebcd2b
Martin Robinson <martinjrobins@gmail.com>
2017-03-27 17:10:54 +0100

    still working on fmm

--- src/FastMultipoleMethod.h
+++ src/FastMultipoleMethod.h
@@ -58,7 +58,6 @@ namespace detail {
         typedef std::array<double,ncheb> vector_type;
 
 
-        inline 
         void L2M(InOutIterator accum_begin, 
                  box_type& box, 
                  PositionIterator position_begin, 
@@ -72,16 +71,15 @@ namespace detail {
             int_d start(0);
             int_d end(N-1);
 
-            for (int i=0; i<np; ++i) {
-                lattice_iterator<D> mj(m_start,m_end,m_start);
-                for (int j=0; j<ncheb; ++j,++mj) {
-                    accum_begin[j] += row_Rn(*mj,i)*source_begin[i];
+            lattice_iterator<D> mi(m_start,m_end,m_start);
+            for (int i=0; i<ncheb; ++i,++mi) {
+                for (int j=0; j<np; ++j) {
+                    accum_begin[i] += row_Rn(*mi,j)*source_begin[j];
                 }
             }
         }
 
 
-        inline 
         void M2M(InOutIterator accum_begin, 
                  box_type& target_box, 
                  box_type& source_box, 
@@ -98,52 +96,85 @@ namespace detail {
                                         + source_box.bmin;
             }
              
-            cheb_rn.calculate_Sn(source_positions.begin(),ncheb,N);
+            cheb_rn.calculate_Sn_with_bbox(source_positions.begin(),target_box,ncheb,N);
 
-            for (int i=0; i<ncheb; ++i) {
-                const double_d pi = 0.5*(cheb_node_unit_box+1)*(box.bmax-box.bmin) + box.bmin;
-                lattice_iterator<D> mj(m_start,m_end,m_start);
-                for (int j=0; j<ncheb; ++j,++mj) {
-                    accum_begin[j] += row_Rn(*mj,i)*source_begin[i];
+            lattice_iterator<D> mi(m_start,m_end,m_start);
+            for (int i=0; i<ncheb; ++i,++mi) {
+                for (int j=0; j<ncheb; ++j) {
+                    accum_begin[i] += cheb_rn(*mi,j)*source_begin[j];
                 }
             }
         }
 
-        template <typename VectorType, unsigned int D, unsigned int N, typename SourceVectorType>
-        void calculate_L2M(vector_type& sum, detail::bbox& my_box, iterator_range<ranges_iterator<traits_type>>& range, sou) {
-            const size_t N = std::distance(range.begin(),range.end());
-            const double_d* pbegin = &get<position>(*range.begin());
-            const size_t pindex = pbegin - &get<position>(source_particles)[0];
-            return Expansions::L2M(sum, my_box,pbegin,pbegin + N,source_vector.begin() + index);  
+
+        template <typename Function>
+        void M2L(InOutIterator accum_begin, 
+                 box_type& target_box, 
+                 box_type& source_box, 
+                 InputIterator source_begin,
+                 Function& K) {
+
+            int_d start(0);
+            int_d end(N-1);
+
+            lattice_iterator<dimension> mi(m_start,m_end,m_start);
+            for (int i=0; i<ncheb; ++i,++mj) {
+                const double_d pi_unit_box = detail::chebyshev_node_nd(*mi);
+                const double_d pi = 0.5*(pi_unit_box+1)*(target_box.bmax-target_box.bmin) 
+                                                                    + target_box.bmin;
+
+                lattice_iterator<dimension> mj(m_start,m_end,m_start);
+                for (int j=0; j<ncheb; ++j,++mj) {
+                    const double_d pj_unit_box = detail::chebyshev_node_nd(*mj);
+                    const double_d pj = 0.5*(pj_unit_box+1)*(source_box.bmax-source_box.bmin) 
+                                                                    + source_box.bmin;
+                    accum_begin[i] += K(pj-pi,pi,pj)*source_begin[j];
+                }
+            }
+
+
         }
 
-        // assume serial processing of particles, this could be more efficient for ranges iterators
-        template <typename Iterator, typename = typename
-            std::enable_if<!std::is_same<Iterator,ranges_iterator<traits_type>>>
-            void calculate_L2M(vector_type& sum, detail::bbox& my_box, iterator_range<Iterator>& range) {
+     
 
-                const size_t N = std::distance(range.begin(),range.end());
-                std::vector<double_d> positions(N);
-                std::transform(range.begin(),range.end(),positions.begin(),
-                        [](const_particle_reference p) { return get<position>(p); });
-                std::vector<double> source_values(N);
-                std::transform(range.begin(),range.end(),source_values.begin(),
-                        [](const_particle_reference p) { 
-                        const size_t index = (&get<position>(p))-(&get<position>(source_particles)[0]); 
-                        return source_vector[index];
-                        });
+    };
 
-                Expansions::L2M(sum, my_box,positions.begin(), positions.end(), source_values.begin());
 
-            }
+    template <typename VectorType, unsigned int D, unsigned int N, typename SourceVectorType>
+    void calculate_L2M(vector_type& sum, detail::bbox& my_box, iterator_range<ranges_iterator<traits_type>>& range, sou) {
+        const size_t N = std::distance(range.begin(),range.end());
+        const double_d* pbegin = &get<position>(*range.begin());
+        const size_t pindex = pbegin - &get<position>(source_particles)[0];
+        return Expansions::L2M(sum, my_box,pbegin,pbegin + N,source_vector.begin() + index);  
+    }
+
+    // assume serial processing of particles, this could be more efficient for ranges iterators
+    template <typename Iterator, typename = typename
+        std::enable_if<!std::is_same<Iterator,ranges_iterator<traits_type>>>
+        void calculate_L2M(vector_type& sum, detail::bbox& my_box, iterator_range<Iterator>& range) {
+
+            const size_t N = std::distance(range.begin(),range.end());
+            std::vector<double_d> positions(N);
+            std::transform(range.begin(),range.end(),positions.begin(),
+                    [](const_particle_reference p) { return get<position>(p); });
+            std::vector<double> source_values(N);
+            std::transform(range.begin(),range.end(),source_values.begin(),
+                    [](const_particle_reference p) { 
+                    const size_t index = (&get<position>(p))-(&get<position>(source_particles)[0]); 
+                    return source_vector[index];
+                    });
+
+            Expansions::L2M(sum, my_box,positions.begin(), positions.end(), source_values.begin());
+
+        }
     }
             
 
 
-template <typename Expansions, typename SourceNeighbourQuery, 
+template <typename Expansions, typename NeighbourQuery, 
           typename SourceVectorType, typename StorageVectorType>
-struct calculate_S_expansion {
-    SourceNeighbourQuery &m_search;
+struct calculate_L2M_and_M2M {
+    NeighbourQuery &m_search;
     SourceVectorType &m_source_vector;
     StorageVectorType &m_W;
     typedef typename SourceNeighbourQuery::reference reference;
@@ -161,9 +192,9 @@ struct calculate_S_expansion {
     {}
 
     vector_type& operator()(const reference bucket) {
-        detail::bbox my_box(search.get_bucket_bounds_low(bucket),
-                            search.get_bucket_bounds_high(bucket));
-        size_t my_index = search.get_index(bucket);
+        detail::bbox my_box(m_search.get_bucket_bounds_low(bucket),
+                            m_search.get_bucket_bounds_high(bucket));
+        size_t my_index = m_search.get_index(bucket);
         vector_type& W = m_W[my_index];
 
         if (search.is_leaf_node(bucket) { // leaf node
@@ -185,113 +216,111 @@ struct calculate_S_expansion {
     }
 };
 
-template <typename Expansions, typename StorageVectorType
-          typename SourceNeighbourQuery, typename TargetNeighbourQuery>
-struct translate_S_R_accumulator {
-    TargetNeighbourQuery &m_target;
-    SourceNeighbourQuery &m_source;
-    StorageVectorType &m_W;
-    const static unsigned int dimension = TargetNeighbourQuery::dimension;
-    detail::bbox<Dtarget> m_target_bbox;
-    typedef typename SourceNeighbourQuery::bucket_reference reference;
+template <typename Expansions, typename NeighbourQuery, 
+          typename SourceVectorType, typename StorageVectorType>
+struct calculate_L2L {
+    const NeighbourQuery &m_search;
+    const SourceVectorType &m_source_vector;
+    StorageVectorType &m_g;
+    typedef typename NeighbourQuery::reference reference;
     typedef Expansions::vector_type vector_type;
-    vector_type &m_sum;
-
-    evaluate_B_l(SourceNeighbourQuery& source,
-                 TargetNeighbourQuery& target,
-                 detail::bbox<Dtarget>& target_bbox,
-                 StorageVectorType& W,
-                 vector_type& sum)
-        :q(q),target(target),source(source),m_W(W),m_target_bbox(target_bbox),m_sum {}
-
-    vector_type operator()(const reference source_bucket) {
-        detail::bbox<dimension> source_bbox(source.get_bucket_bounds_low(source_bucket),
-                                          source.get_bucket_bounds_high(source_bucket));
-        size_t source_index = m_source.get_index(source_bucket); 
-        Expansions::M2L(m_sum.begin(),m_target_bbox,source_bbox,m_W[source_index].begin());
-    }
-};
- 
-
-template <typename Expansions, typename StorageVectorType 
-          typename SourceNeighbourQuery, typename TargetNeighbourQuery>
-struct translate_S_R {
-    TargetNeighbourQuery &m_target;
-    SourceNeighbourQuery &m_source;
-    StorageVectorType& m_W;
-    StorageVectorType& m_g;
-    const static unsigned int dimension = SourceNeighbourQuery::dimension;
-    typedef typename TargetNeighbourQuery::bucket_reference reference;
-    typedef Expansions::vector_type vector_type;
-    typedef iterator_range<SourceNeighbourQuery::well_separated_iterator> well_separated_iterator_range_type;
-
-    evaluate_B_l(SourceNeighbourQuery &source,
-                 TargetNeighbourQuery &target,
-                 StorageVectorType& W,
-                 StorageVectorType& g)
-        :m_target(target),m_source(source),m_W(W),m_g(g) {}
-
-    void operator()(const reference target_bucket) {
-        detail::bbox<dimension> target_bbox(m_target.get_bucket_bounds_low(target_bucket),
-                                          m_target.get_bucket_bounds_high(target_bucket));
-        size_t target_index = m_target.get_index(target_bucket); 
-        well_separated_iterator_range_type source_buckets 
-                    = source.get_well_separated_buckets(target_bbox);
-        std::for_each(source_buckets.begin(),
-                      source_buckets.end(),
-                      translate_S_R_accumulator(m_source,m_target,target_bbox,m_W,m_g[my_index]));
-        
-    }
-};
-   
-template <typename Traits, typename Expansions,
-         typename SourceNeighbourQuery, typename TargetNeighbourQuery>
-class FastMultipoleMethod {
     typedef SourceNeighbourQuery::traits_type traits_type;
-    typedef ExpansionsSource::vector_type vector_type;
+    static const unsigned int dimension = traits_type::dimension;
+    
+    calculate_S_expansion(NeighbourQuery& search, 
+                          SourceVectorType& source_vector,
+                          StorageVectorType& g):
+        m_search(search),
+        m_source_vector(source_vector),
+        m_g(g)
+    {}
+
+    void calculate_L2L_dive(const vector_type& g_parent, detail::bbox<dimension>& bbox_parent, const reference bucket) {
+        detail::bbox box(m_search.get_bucket_bounds_low(bucket),
+                        m_search.get_bucket_bounds_high(bucket));
+        size_t index = m_search.get_index(bucket);
+        vector_type& g = m_g[index];
+        Expansions::L2L(g.begin(),box,bbox_parent,g_parent.begin());
+        if (search.is_leaf_node(bucket) { // leaf node
+            calculate_L2L_dive(g,box,m_search.get_child1(bucket));
+            calculate_L2L_dive(g,box,m_search.get_child2(bucket));
+        }
+    }
+
+    vector_type& operator()(const reference bucket) {
+        if (search.is_leaf_node(bucket) { // leaf node
+            detail::bbox box(m_search.get_bucket_bounds_low(bucket),
+                        m_search.get_bucket_bounds_high(bucket));
+            size_t my_index = m_search.get_index(bucket);
+            const vector_type& g = m_g[my_index];
+            calculate_L2L_dive(g,box,m_search.get_child1(bucket));
+            calculate_L2L_dive(g,box,m_search.get_child2(bucket));
+        }
+    }
+
+};
+
+   
+template <typename Expansions, typename NeighbourQuery>
+class FastMultipoleMethod {
+    typedef NeighbourQuery::traits_type traits_type;
+    typedef Expansions::vector_type vector_type;
     typedef traits_type::template vector_type<vector_type> storage_type;
-    typedef iterator_range<SourceNeighbourQuery::root_iterator> source_root_iterator_range;
+    typedef iterator_range<NeighbourQuery::root_iterator> root_iterator_range_type;
+    typedef iterator_range<NeighbourQuery::all_iterator> all_iterator_range_type;
     storage_type m_W;
     storage_type m_g;
-    SourceNeighbourQuery *m_source;
+    NeighbourQuery *m_source;
 
 public:
 
-    SingleLevelFMM(SourceParticlesQuery &source):
+    FastMultipoleMethod(NeighbourQuery &source):
         m_source(&source)
     {}
 
-    template <typename SourceVectorType>
-    calculate_expansions(SourceVectorType& source_vector) {
-        source_root_iterator_range source_root_buckets = m_source->get_root_buckets();
-        source_all_iterator_range source_all_buckets = m_source.get_all_buckets();
+    template <typename Function, typename VectorType>
+    calculate_expansions(const Function& K, const VectorType& source_vector) {
+        root_iterator_range_type root_buckets = m_source->get_root_buckets();
+        all_iterator_range_type all_buckets = m_source->get_all_buckets();
 
         m_W.resize(m_source->number_of_buckets());
         m_g.resize(m_source->number_of_buckets());
 
-        // calculate L2M and M2M expansions for source buckets
-        std::for_each(source_root_buckets.begin(),
-                      source_root_buckets.end(),
-                      calculate_S_expansion<ExpansionsSource,SourceNeighbourQuery,
-                                            SourceVectorType,source_storage_type>(
+        // calculate L2M and M2M expansions for source buckets (recursive up
+        // the tree so use function object)
+        std::for_each(root_buckets.begin(),
+                      root_buckets.end(),
+                      calculate_P2M_and_M2M<Expansions,NeighbourQuery,
+                                            VectorType,storage_type>(
                                                 *m_source,
                                                 source_vector,
                                                 m_W));
 
 
-        // calculate M2L translations for source buckets
-        std::for_each(source_all_buckets.begin(),
-                      source_all_buckets.end(),
-                      translate_S_R<Expansions,StorageVectorType,
-                                    SourceNeighbourQuery,TargetNeighbourQuery>(
-                              m_source
-                              m_W,m_g));
+        // calculate M2L translations for source buckets (this should be the most work)
+        std::for_each(all_buckets.begin(),all_buckets.end(),
+                      [&](const reference target_bucket) {
+            detail::bbox<dimension> target_bbox(m_source->get_bucket_bounds_low(target_bucket),
+                                          m_target->get_bucket_bounds_high(target_bucket));
+            size_t target_index = m_source->get_index(target_bucket); 
+            well_separated_iterator_range_type source_buckets 
+                    = m_source->get_well_separated_buckets(target_bucket);
+            std::for_each(source_buckets.begin(), source_buckets.end(), 
+                    [&](const reference source_bucket) {
+                detail::bbox<dimension> source_bbox(m_source->get_bucket_bounds_low(source_bucket),
+                                                  m_source->get_bucket_bounds_high(source_bucket));
+                size_t source_index = m_source->get_index(source_bucket); 
+                Expansions::M2L(m_sum.begin(),m_target_bbox,source_bbox,m_W[source_index].begin(),K)
+            });
+        });
 
-        // calculate L2L translations for source buckets
-        std::for_each(source_root_buckets.begin(),
-                      source_root_buckets.end(),
-                      calculate_R_expansion<ExpansionsSource,SourceNeighbourQuery,
-                                            SourceVectorType,source_storage_type>(
+
+        // calculate L2L translations for source buckets (recursive 
+        // down the tree so use function object)
+        std::for_each(root_buckets.begin(),
+                      root_buckets.end(),
+                      calculate_L2L<Expansions,NeighbourQuery,
+                                            VectorType,storage_type>(
                                                 *m_source,
                                                 source_vector,
                                                 m_W));
@@ -301,7 +330,13 @@ public:
 
     // evaluate expansions for given point
     Expansion::Scalar evaluate_R_expansion(const double_d& p) {
-        target_neighbouring_iterator_range neighbouring_buckets = m_source->get_neighbouring_buckets(p);
+        const reference bucket = m_source->get_bucket(p);
+        const size_t index = m_source->get_index(bucket); 
+        detail::bbox<dimension> box(m_source->get_bucket_bounds_low(bucket),
+                                    m_source->get_bucket_bounds_high(bucket));
+        target_neighbouring_iterator_range neighbouring_buckets = m_source->get_neighbouring_buckets(bucket);
+
+        Expansions::L2P(p,m_target_bbox,source_bbox,m_W[source_index].begin(),K)
         
 
     }
