8298930cc57dc8d08c9f5ab0b8254704a3851a10
martinjrobins <martinjrobins@gmail.com>
2018-06-10 07:28:27 +0100

    #29 remove CMAKE_EXE_LINKER_FLAGS cmake variable as this is no longer set by FindOpenMP.cmake

--- tests/doc_getting_started.h
+++ tests/doc_getting_started.h
@@ -303,8 +303,6 @@ public:
     find_package(OpenMP REQUIRED)
     add_definitions(-DHAVE_OPENMP)
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${OpenMP_CXX_FLAGS}")
-    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS}
-    ${OpenMP_EXE_LINKER_FLAGS}")
 
     # Thrust
     find_package(Thrust REQUIRED)
@@ -439,8 +437,6 @@ public:
     find_package(OpenMP REQUIRED)
     add_definitions(-DHAVE_OPENMP)
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${OpenMP_CXX_FLAGS}")
-    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS}
-    ${OpenMP_EXE_LINKER_FLAGS}")
 
     # CUDA
     find_package(CUDA REQUIRED)
